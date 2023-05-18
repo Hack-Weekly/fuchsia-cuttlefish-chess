a14514dad31d893298212f01870d860bfd39add6
JeffLi117 <li.jeffreyt@yahoo.com>
2023-05-18 18:09:04 -0500

    Added linking for each join button lobby on main page

---
+++ client/poker-app/src/components/InsideRoom.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function InsideRoom() {
+  return (
+    <div>
+        <div>Seeing this div means I should see each individual room</div>
+
+    </div>
+  )
+}
+
+export default InsideRoom
--- client/poker-app/src/components/RoomsShown.jsx
+++ client/poker-app/src/components/RoomsShown.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import EachRoom from './EachRoom'
-
+import InsideRoom from './InsideRoom'
 
 const randomSet1 = {
     name: "Welcome All",
@@ -19,36 +19,41 @@ const randomSet2 = {
 export default function RoomsShown() {
   return (
     <div>
-
         <EachRoom
             name={randomSet1.name}
             currentPlayers={randomSet1.currentPlayers}
             max={randomSet1.max}
             gameid={randomSet1.gameid}
+            roomNum={1}
+            
         />
         <EachRoom
             name={randomSet1.name}
             currentPlayers={randomSet1.currentPlayers}
             max={randomSet1.max}
             gameid={randomSet1.gameid}
+            roomNum={2}
         />
         <EachRoom
             name={randomSet2.name}
             currentPlayers={randomSet2.currentPlayers}
             max={randomSet2.max}
             gameid={randomSet2.gameid}
+            roomNum={3}
         />
         <EachRoom
             name={randomSet2.name}
             currentPlayers={randomSet2.currentPlayers}
             max={randomSet2.max}
             gameid={randomSet2.gameid}
+            roomNum={4}
         />
         <EachRoom
             name={randomSet1.name}
             currentPlayers={randomSet1.currentPlayers}
             max={randomSet1.max}
             gameid={randomSet1.gameid}
+            roomNum={5}
         />
     </div>
   )
